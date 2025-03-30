5eb677e0ab779ece468456b232ee05c034255492
Dawid <dawidekpe@gmail.com>
2025-03-30 18:45:25 +0200

    test gtoa

--- src/main.cs
+++ src/main.cs
@@ -7,60 +7,46 @@ class GrayWolfOptimizerProgram {
 		uint[] dimensionCounts = { 2, 3, 6, 10 };
 		uint[] iterationCounts = { 5, 10, 20, 40, 60, 80 };
 		uint[] populationSizes = { 10, 20, 40, 80 };
-		double[] maxApproachFactors = { 2.0 };
-		double[] obstacleFactors = { 2.0 };
+		double[] teachingFactors = { 1.0, 2.0 };
 
-		// var testResults = new List<Tester<GWO.Optimizer, GWO.Params>.TestResult>();
+		var testResults = new List<Tester<GTOA.Optimizer, GTOA.Params>.TestResult>();
 
-		uint dimensions = 2;
+		foreach(uint dimensions in dimensionCounts) {
+			EvaluationFunction function = new RastriginFunction();
+			Vector displacement = Vector.Ones(dimensions);
+			displacement.vals[0]++;
+			function.Displacement = displacement;
 
-		Range searchRange = new Range(
-			Vector.Ones(dimensions) * -10.0,
-			Vector.Ones(dimensions) * 12.0
-		);
+			Range searchRange = new Range(
+				Vector.Ones(dimensions) * -10.0,
+				Vector.Ones(dimensions) * 12.0
+			);
 
-		EvaluationFunction function = new RosenbrockFunction();
-		function.Displacement = new Vector(new double[] { 0, 1 });
+			var optimizer = new GTOA.Optimizer(function, searchRange, dimensions);
 
-		var optimizer = new GTOA.Optimizer(function, searchRange, dimensions);
+			var tester = new Tester<GTOA.Optimizer, GTOA.Params>(
+				optimizer,
+				function,
+				searchRange
+			);
 
-		var minValue = optimizer.Solve();
-		var bestAgent = optimizer.BestAgent;
+			Tester<GTOA.Optimizer, GTOA.Params>.TestResult[] currentResults = tester.RunTests(
+				Tester<GTOA.Optimizer, GTOA.Params>.TestCase.Combinations(
+					new uint[] { dimensions },
+					iterationCounts,
+					populationSizes,
+					GTOA.Params.Combinations(
+						teachingFactors
+					)
+				)
+			);
 
-		Console.WriteLine(function.Name + " minimum: " + minValue + " (" + bestAgent.Position + ")");
+			foreach(var result in currentResults) {
+				testResults.Add(result);
+			}
+		}
 
-		// foreach(uint dimensions in dimensionCounts) {
-		// 	Range searchRange = new Range(
-		// 		Vector.Ones(dimensions) * -10.0,
-		// 		Vector.Ones(dimensions) * 12.0
-		// 	);
-
-		// 	var optimizer = new GWO.Optimizer(function, searchRange, dimensions);
-
-		// 	var tester = new Tester<GWO.Optimizer, GWO.Params>(
-		// 		optimizer,
-		// 		function,
-		// 		searchRange
-		// 	);
-
-		// 	Tester<GWO.Optimizer, GWO.Params>.TestResult[] currentResults = tester.RunTests(
-		// 		Tester<GWO.Optimizer, GWO.Params>.TestCase.Combinations(
-		// 			new uint[] { dimensions },
-		// 			iterationCounts,
-		// 			populationSizes,
-		// 			GWO.Params.Combinations(
-		// 				maxApproachFactors,
-		// 				obstacleFactors
-		// 			)
-		// 		)
-		// 	);
-
-		// 	foreach(var result in currentResults) {
-		// 		testResults.Add(result);
-		// 	}
-		// }
-
-		// var outputter = new CSVTestResultOutputter<GWO.Optimizer, GWO.Params>();
-		// outputter.Output(testResults.ToArray());
+		var outputter = new CSVTestResultOutputter<GTOA.Optimizer, GTOA.Params>();
+		outputter.Output(testResults.ToArray());
 	}
 }
